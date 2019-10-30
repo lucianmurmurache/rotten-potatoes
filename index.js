ce1ec96dc34b37c5e07ffb34fd65365de3bd216f
LucianMurmurache <48526455+lucianmurmurache@users.noreply.github.com>
2019-10-30 20:55:45 +0100

    Small correction to index file

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const app = express();
 const http = require('http');
 
 http.createServer((request, response) => {
-  response.writeHead(200, {'Content-Type': 'text/plain')};
-    response.end('Welcome to Fight Club!\n');
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('Welcome to Fight club!\n');
 }).listen(8080);
 
-console.log('My 1st Node test server running on port 8080!');
+console.log('My first Node test server is running on Port 8080.');
