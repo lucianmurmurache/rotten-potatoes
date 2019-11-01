760a87f9e1cc44703d7d2ad2232f8bc766cd8b03
LucianMurmurache <48526455+lucianmurmurache@users.noreply.github.com>
2019-11-01 21:57:37 +0100

    Added express, morgan, top10movies and error handler

--- index.js
+++ index.js
@@ -1,11 +1,67 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
-const http = require('http');
+// Top (10) movies
+let topMovies = [{
+  title: 'The Godfather',
+  director: 'Francis Ford Coppola'
+},
+{
+  title: 'The Lord of the Rings: The Fellowship of the Ring',
+  director: 'Peter Jackson'
+},
+{
+  title: 'The Lord of the Rings: The Two Towers',
+  director: 'Peter Jackson'
+},
+{
+  title: 'The Lord of the Rings: The Return of the King',
+  director: 'Peter Jackson'
+},
+{
+  title: 'The Dark Knight',
+  director: 'Christopher Nolan'
+},
+{
+  title: 'The Dark Knight Rises',
+  director: 'Christopher Nolan'
+},
+{
+  title: 'Avengers: Infinity War',
+  director: 'Anthony and Joe Russo'
+},
+{
+  title: 'Saving Private Ryan',
+  director: 'Steven Spielberg'
+},
+{
+  title: 'Gladiator',
+  director: 'Ridley Scott'
+},
+{
+  title: '12 Angry Men',
+  director: 'Sidney Lumet'
+}]
 
-http.createServer((request, response) => {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Welcome to Fight club!\n');
-}).listen(8080);
+// log requests using morgan
+app.use(morgan('common'));
 
-console.log('My first Node test server is running on Port 8080.');
+// Get
+app.get('/movies', function(req, res) {
+  res.json(topMovies)
+});
+
+// Default textual response
+app.get('/', function(req, res) {
+  res.send('Welcome to Rotten Potatoes!')
+});
+// Serves public folder
+app.use(express.static('public'));
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('Dang it, something broke!');
+});
+
+// Listen for requests - port 8080
+app.listen(8080);
