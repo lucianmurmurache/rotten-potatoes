6e96c3968dd9241731ea14810bd356aecf8e5ced
LucianMurmurache <48526455+lucianmurmurache@users.noreply.github.com>
2019-12-08 11:17:30 +0100

    Indented the code for easier reading

--- models.js
+++ models.js
@@ -3,44 +3,53 @@ const mongoose = require('mongoose');
 const bcryptjs = require('bcryptjs');
 
 var movieSchema = mongoose.Schema({
-  title : {type: String, required: true},
-  description : {type: String, required: true},
-  genre : {
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  genre: {
     name: String,
-    description : String
+    description: String
   },
-  director : {
+  director: {
     Name: String,
-    Bio : String
+    Bio: String
   },
-  actors : [String],
-  imagePath : String,
-  featured : Boolean
+  actors: [String],
+  imagePath: String,
+  featured: Boolean
 });
 
 var userSchema = mongoose.Schema({
-  username : {
+  username: {
     type: String,
     required: true
   },
-  password : {
+  password: {
     type: String,
     required: true
   },
-  email : {
+  email: {
     type: String,
     required: true,
   },
-  birthday : Date,
-  favourites : [{ type : mongoose.Schema.Types.ObjectId, ref: 'movies'}]
+  birthday: Date,
+  favourites: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'movies'
+  }]
 });
 // Hash password entered when registering, before it is stored in MongoDB.
-userSchema.statics.hashPassword = function(password) {
+userSchema.statics.hashPassword = function (password) {
   return bcryptjs.hashSync(password, 10);
 };
 
-userSchema.methods.validatePassword = function(password) {
-    return bcryptjs.compareSync(password, this.password);
+userSchema.methods.validatePassword = function (password) {
+  return bcryptjs.compareSync(password, this.password);
 };
 
 var Movie = mongoose.model('Movie', movieSchema);
