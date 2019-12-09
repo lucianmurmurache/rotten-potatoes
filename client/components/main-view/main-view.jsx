6f3519bf94adeb7a15c477220b6d1691870a4ddf
lucianmurmurache <lucianmurmurache@gmail.com>
2019-12-09 21:29:47 +0100

    Added login to show movie list(movies endpoint)

---
+++ client/components/main-view/main-view.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import axios from 'axios';
+
+export class MainView extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {};
+    }
+
+    componentDidMount() {
+        axios.get('<https://rotten-potatoes3000.herokuapp.com/movies>') // This might require correction!('</movies>')
+            .then(response => {
+                //Assign result to state
+                this.setState({
+                    movies: response.data
+                });
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    render() {
+        const { movies } = this.state;
+
+        if (!movies) return <div className="main-view" />;
+
+        return (
+            <div className="main-view">
+                {movies.map(movie => (
+                    <div className="movie-card" key={movie._id}>{movie.Title}</div>
+                ))}
+            </div>
+        );
+    }
+}
