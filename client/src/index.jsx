aa1848c41aee696b37ba51f6a90592d7e64aa4ec
lucianmurmurache <lucianmurmurache@gmail.com>
2019-12-09 21:27:21 +0100

    Added import MainView

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,28 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import axios from 'axios';
+
+import { MainView } from './components/main-view/main-view';
 
 // Bundle ./index.scss
 import "./index.scss";
 
 //Main component
 class RottenPotatoes extends React.Component {
-  constructor() {
-    //Call superclass constructor
-    super();
-    //Initialise the state to an empty object which is to be destructured later
-    this.state = {};
-  }
-
   render() {
-    return (
-      <div className="main-view"></div>
-    );
+    return <MainView />;
   }
 }
 
-//Finds the root of the app
+//Find the root of the app
 const container = document.getElementsByClassName("app-container")[0];
 
-// Tells React to render the app in the room DOM element
+// Tell React to render the app in the room DOM element
 ReactDOM.render(React.createElement(RottenPotatoes), container);
