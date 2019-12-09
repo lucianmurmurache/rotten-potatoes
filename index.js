a007a96c1cb786d23363a36b401b63dd15826f9e
lucianmurmurache <lucianmurmurache@gmail.com>
2019-12-09 20:12:51 +0100

    Allow unregistered users to see movie list

--- index.js
+++ index.js
@@ -52,9 +52,7 @@ app.get('/', function (req, res) {
 });
 
 // GET full movie list
-app.get('/movies', passport.authenticate('jwt', {
-  session: false
-}), function (req, res) {
+app.get('/movies', function (req, res) {
   Movies.find()
     .then(function (movies) {
       res.status(201).json(movies)
