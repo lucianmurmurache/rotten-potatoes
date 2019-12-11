03e5c80840c9b5b78f92430f186b0e8ad1d89793
LucianMurmurache <48526455+lucianmurmurache@users.noreply.github.com>
2019-12-11 21:04:56 +0100

    Added import MovieCard and updated return

--- client/components/main-view/main-view.jsx
+++ client/components/main-view/main-view.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 
+import { MovieCard } from '../movie-card/movie-card';
+
 export class MainView extends React.Component {
 
     constructor() {
@@ -30,7 +32,7 @@ export class MainView extends React.Component {
         return (
             <div className="main-view">
                 {movies.map(movie => (
-                    <div className="movie-card" key={movie._id}>{movie.title}</div>
+                    <MovieCard key={movie._id} movie={movie} />
                 ))}
             </div>
         );
