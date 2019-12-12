66991be1e11b165068d4f2c14013dcf67878fec0
LucianMurmurache <48526455+lucianmurmurache@users.noreply.github.com>
2019-12-12 19:40:01 +0100

    Placed file in the correct folder

---
+++ client/src/components/main-view/main-view.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import axios from 'axios';
+
+import { MovieCard } from '../movie-card/movie-card';
+import { MovieView } from '../movie-view/movie-view';
+
+export class MainView extends React.Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            movies: null,
+            selectedMovie: null
+        };
+    }
+
+    componentDidMount() {
+        axios.get('<https://rotten-potatoes3000.herokuapp.com/movies>')
+            .then(response => {
+                //Assign result to state
+                this.setState({
+                    movies: response.data
+                });
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    onMovieClick(movie) {
+        this.setState({
+            selectedMovie: movie
+        });
+    }
+
+    render() {
+        const { movies, selectedMovie } = this.state;
+
+        if (!movies) return <div className="main-view" />;
+
+        return (
+            <div className="main-view">
+                {selectedMovie
+                    ? <MovieView movie={selectedMovie} />
+                    : movies.map(movie => (
+                        <MovieCard key={movie._id} movie={movie} onClick={movie => this.onMovieClick(movie)} />
+                    ))
+                }
+            </div>
+        );
+    }
+}
