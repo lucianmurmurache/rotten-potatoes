3ca06957e5bd084dbd0233ebf4aa6f517bb30c50
LucianMurmurache <48526455+lucianmurmurache@users.noreply.github.com>
2019-12-17 22:00:02 +0100

    Updated to be checked 3.4

--- client/src/components/registartion-view/registration-view.jsx
+++ client/src/components/registartion-view/registration-view.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 
 export function RegistrationView(props) {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(username, password);
+        /* 
+            Send request to server for authentication 
+            and then call props.onLoggedIn(username)
+        */
+        props.onLoggedIn(username);
+    };
+
+
     return (
         <Form>
             <Form.Group as={Row} controlId="formHorizontalEmail">
                 <Form.Label column sm={2}>
-                    Email
+                    Username:
+                        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
                 </Form.Label>
                 <Col sm={10}>
                     <Form.Control type="email" placeholder="Email" />
@@ -15,7 +30,9 @@ export function RegistrationView(props) {
 
             <Form.Group as={Row} controlId="formHorizontalPassword">
                 <Form.Label column sm={2}>
-                    Password
+                    Password:
+                        <input type="password" value={password} onChange={e => setPassword(e.target.value)}
+                    />
                 </Form.Label>
                 <Col sm={10}>
                     <Form.Control type="password" placeholder="Password" />
