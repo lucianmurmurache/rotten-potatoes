27879e1ebfac3d950c4312c56ef553dbb96cf9bb
LucianMurmurache <48526455+lucianmurmurache@users.noreply.github.com>
2019-12-18 21:52:06 +0100

    Added passport authenticate to movies endpoint

--- index.js
+++ index.js
@@ -52,7 +52,9 @@ app.get('/', function (req, res) {
 });
 
 // GET full movie list
-app.get('/movies', function (req, res) {
+app.get('/movies', passport.authenticate('jwt', {
+  session: false
+}), function (req, res) {
   Movies.find()
     .then(function (movies) {
       res.status(201).json(movies)
