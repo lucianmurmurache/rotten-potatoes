9fda8bd8c8cb08a684826c91e8d217035991abd9
LucianMurmurache <48526455+lucianmurmurache@users.noreply.github.com>
2019-12-18 21:50:20 +0100

    Úpdated handleSubmit and form

--- client/src/components/login-view/login-view.jsx
+++ client/src/components/login-view/login-view.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import axios from 'axios';
 
 
 export function LoginView(props) {
@@ -9,25 +10,38 @@ export function LoginView(props) {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(username, password);
-        /* 
-            Send request to server for authentication 
-            and then call props.onLoggedIn(username)
-        */
-        props.onLoggedIn(username);
+        /* Send request to server for authentication */
+        axios.post('https://rotten-potatoes3000.herokuapp.com/login', {
+            Username: username,
+            Password: password
+        })
+            .then(response => {
+                const data = response.data;
+                props.onLoggedIn(data);
+            })
+            .catch(e => {
+                console.log('no such user')
+            });
     };
 
     return (
         <Form>
-            <Form.Label column sm={2}>
-                Username:
-                <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-            </Form.Label>
-            <Form.Label column sm={2}>
-                Password:
-                <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-            </Form.Label>
-            <Button variant="dark" type="button" onClick={handleSubmit}>Submit</Button>
+            <Form.Group controlId="formHorizontalUsername">
+                <Form.Label column sm={2}>
+                    Username
+                </Form.Label>
+                <Form.Control type="text" placeholder="Enter username" value={username} onChange={e => setUsername(e.target.value)} />
+            </Form.Group>
+
+            <Form.Group controlId="formHorizontalPassword">
+                <Form.Label column sm={2}>
+                    Password
+                </Form.Label>
+                <Form.Control type="password" placeholder="Enter password" value={password} onChange={e => setPassword(e.target.value)} />
+            </Form.Group>
+            <Form.Group>
+                <Button variant="dark" type="button" onClick={handleSubmit}>Submit</Button>
+            </Form.Group>
         </Form>
     );
 }
