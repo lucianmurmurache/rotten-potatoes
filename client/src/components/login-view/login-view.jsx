4057e5a274a84f8ab1b3394296d9043b5e541c27
LucianMurmurache <48526455+lucianmurmurache@users.noreply.github.com>
2019-12-21 22:07:44 +0100

    Added Row and Col to login-view

--- client/src/components/login-view/login-view.jsx
+++ client/src/components/login-view/login-view.jsx
@@ -1,8 +1,14 @@
 import React, { useState } from 'react';
-import '../login-view/login-view.scss';
-import Button from 'react-bootstrap/Button';
+
+/* =============react-bootstrap=============*/
 import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+/* =============react-bootstrap=============*/
+
 import axios from 'axios';
+import './login-view.scss';
 
 
 export function LoginView(props) {
@@ -26,24 +32,28 @@ export function LoginView(props) {
     };
 
     return (
-        <Form className="login-form">
-            <Form.Label className="login-intro">User Login</Form.Label>
-            <Form.Group controlId="formBasicUsername">
-                <Form.Label>
-                    Username
+        <Row className="login-row">
+            <Col xs={10} sm={8} md={6} className="form-col">
+                <Form className="login-form">
+                    <Form.Label className="login-intro">User Login</Form.Label>
+                    <Form.Group controlId="formBasicUsername">
+                        <Form.Label>
+                            Username
                 </Form.Label>
-                <Form.Control type="text" placeholder="Enter username" value={username} autoComplete="on" onChange={(e) => setUsername(e.target.value)} />
-            </Form.Group>
+                        <Form.Control type="text" placeholder="Enter username" value={username} autoComplete="on" onChange={(e) => setUsername(e.target.value)} />
+                    </Form.Group>
 
-            <Form.Group controlId="formBasicPassword">
-                <Form.Label>
-                    Password
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>
+                            Password
                 </Form.Label>
-                <Form.Control type="password" placeholder="Enter password" value={password} autoComplete="off" onChange={(e) => setPassword(e.target.value)} />
-            </Form.Group>
-            <Form.Group>
-                <Button className="login-button" variant="dark" type="button" onClick={handleSubmit}>Login</Button>
-            </Form.Group>
-        </Form>
+                        <Form.Control type="password" placeholder="Enter password" value={password} autoComplete="off" onChange={(e) => setPassword(e.target.value)} />
+                    </Form.Group>
+                    <Form.Group>
+                        <Button className="login-button" variant="dark" type="button" onClick={handleSubmit}>Login</Button>
+                    </Form.Group>
+                </Form>
+            </Col>
+        </Row>
     );
 }
