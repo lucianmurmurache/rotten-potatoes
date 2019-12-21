7459cbaa8f5131c850eac7afcd9c3d45f6069466
LucianMurmurache <48526455+lucianmurmurache@users.noreply.github.com>
2019-12-21 22:11:55 +0100

    Added Row to registration-form

--- client/src/components/registartion-view/registration-view.jsx
+++ client/src/components/registartion-view/registration-view.jsx
@@ -1,7 +1,15 @@
 import React, { useState } from 'react';
+
+/* =============react-bootstrap=============*/
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+//import Col from 'react-bootstrap/Col';
+/* =============react-bootstrap=============*/
+
 import axios from 'axios';
+
+import './registration-view.scss';
 
 export function RegistrationView(props) {
     const [username, createUsername] = useState('');
@@ -29,39 +37,42 @@ export function RegistrationView(props) {
 
 
     return (
-        <Form className="registration-form">
-            <Form.Group controlId="formBasicUsername">
-                <Form.Label column md={2}>
-                    Username
+        <Row className="registration-row">
+            <Form className="registration-form">
+                <Form.Label className="registration-intro">User registration</Form.Label>
+                <Form.Group controlId="formBasicUsername">
+                    <Form.Label column md={2}>
+                        Username
                         <Form.Control type="text" value={username} placeholder="Enter Username" onChange={(e) => createUsername(e.target.value)} />
-                </Form.Label>
-            </Form.Group>
+                    </Form.Label>
+                </Form.Group>
 
-            <Form.Group controlId="formBasicPassword">
-                <Form.Label column md={2}>
-                    Password
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label column md={2}>
+                        Password
                         <Form.Control type="text" value={password} placeholder="Enter Password" onChange={(e) => createPassword(e.target.value)} />
-                </Form.Label>
-            </Form.Group>
+                    </Form.Label>
+                </Form.Group>
 
-            <Form.Group controlId="formBasicEmail">
-                <Form.Label column md={2}>
-                    Email
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label column md={2}>
+                        Email
                     <Form.Control type="text" value={email} placeholder="Enter email" onChange={(e) => createEmail(e.target.value)} />
-                </Form.Label>
-            </Form.Group>
+                    </Form.Label>
+                </Form.Group>
 
-            <Form.Group controlId="formBasicBirthday">
+                <Form.Group controlId="formBasicBirthday">
 
-                <Form.Label column md={2}>
-                    Birthday
+                    <Form.Label column md={2}>
+                        Birthday
                     <Form.Control type="text" value={birthday} placeholder="Enter Birthdate" onChange={(e) => createBirthday(e.target.value)} />
-                </Form.Label>
-            </Form.Group>
+                    </Form.Label>
+                </Form.Group>
 
-            <Form.Group>
-                <Button type="submit">Sign in</Button>
-            </Form.Group>
-        </Form>
+                <Form.Group>
+                    <Button variant="dark" className="registration-button" type="submit" onclick={handleRegister}>Register</Button>
+                </Form.Group>
+            </Form>
+        </Row>
     )
 }
