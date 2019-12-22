a39b2362376b23afe8c58fc07988715c668f3113
LucianMurmurache <48526455+lucianmurmurache@users.noreply.github.com>
2019-12-22 18:32:23 +0100

    Added PropTypes, updated onClick button - handleSubmit

--- client/src/components/registartion-view/registration-view.jsx
+++ client/src/components/registartion-view/registration-view.jsx
@@ -1,26 +1,26 @@
 import React, { useState } from 'react';
+import axios from 'axios';
+import PropTypes from 'prop-types';
+import './registration-view.scss';
 
-/* =============react-bootstrap=============*/
+/* =============react-bootstrap-imports=============*/
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 //import Col from 'react-bootstrap/Col';
-/* =============react-bootstrap=============*/
-
-import axios from 'axios';
-
-import './registration-view.scss';
+//import Container from 'react-bootstrap/Container';
+/* =============react-bootstrap-imports=============*/
 
 export function RegistrationView(props) {
-    const [username, createUsername] = useState('');
-    const [password, createPassword] = useState('');
-    const [email, createEmail] = useState('');
-    const [birthday, createBirthday] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('');
+    const [birthday, setBirthday] = useState('');
 
 
-    const handleRegister = (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault();
-        axios.post('https://rotten-potatoes3000.herokuapp.com/login', {
+        axios.post('https://rotten-potatoes3000.herokuapp.com/user', {
             Username: username,
             Password: password,
             Email: email,
@@ -36,28 +36,28 @@ export function RegistrationView(props) {
     };
 
 
-    return (
+    return ( //Button to switch to login not finished!
         <Row className="registration-row">
             <Form className="registration-form">
                 <Form.Label className="registration-intro">User registration</Form.Label>
                 <Form.Group controlId="formBasicUsername">
                     <Form.Label column md={2}>
                         Username
-                        <Form.Control type="text" value={username} placeholder="Enter Username" onChange={(e) => createUsername(e.target.value)} />
+                        <Form.Control type="text" value={username} placeholder="Enter Username" onChange={(e) => setUsername(e.target.value)} />
                     </Form.Label>
                 </Form.Group>
 
                 <Form.Group controlId="formBasicPassword">
                     <Form.Label column md={2}>
                         Password
-                        <Form.Control type="text" value={password} placeholder="Enter Password" onChange={(e) => createPassword(e.target.value)} />
+                        <Form.Control type="text" value={password} placeholder="Enter Password" onChange={(e) => setPassword(e.target.value)} />
                     </Form.Label>
                 </Form.Group>
 
                 <Form.Group controlId="formBasicEmail">
                     <Form.Label column md={2}>
                         Email
-                    <Form.Control type="text" value={email} placeholder="Enter email" onChange={(e) => createEmail(e.target.value)} />
+                    <Form.Control type="text" value={email} placeholder="Enter email" onChange={(e) => setEmail(e.target.value)} />
                     </Form.Label>
                 </Form.Group>
 
@@ -65,14 +65,23 @@ export function RegistrationView(props) {
 
                     <Form.Label column md={2}>
                         Birthday
-                    <Form.Control type="text" value={birthday} placeholder="Enter Birthdate" onChange={(e) => createBirthday(e.target.value)} />
+                    <Form.Control type="text" value={birthday} placeholder="Enter Birthdate" onChange={(e) => setBirthday(e.target.value)} />
                     </Form.Label>
                 </Form.Group>
 
                 <Form.Group>
-                    <Button variant="dark" className="registration-button" type="submit" onclick={handleRegister}>Register</Button>
+                    <Button variant="dark" className="registration-button" type="submit" onclick={handleSubmit}>Register</Button>
+                </Form.Group>
+                <Form.Group>
+                    <Button variant="link" onclick={handleSubmit}>Already have an account?</Button>
                 </Form.Group>
             </Form>
         </Row>
     )
 }
+
+/*=================PropTypes=================*/
+RegistrationView.propTypes = {
+    onClick: PropTypes.func.isRequired
+};
+/*=================PropTypes=================*/
