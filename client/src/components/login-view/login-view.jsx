65312eddcfa5ad5037d435ca5206f9ad12ed38d0
LucianMurmurache <48526455+lucianmurmurache@users.noreply.github.com>
2019-12-29 15:31:00 +0100

    Added link to switch to registrationView and updated form

--- client/src/components/login-view/login-view.jsx
+++ client/src/components/login-view/login-view.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
+import { Link } from 'react-router-dom';
+
 /* =============react-bootstrap-imports=============*/
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+//import Row from 'react-bootstrap/Row';
+//import Col from 'react-bootstrap/Col';
 /* =============react-bootstrap-imports=============*/
 
 import axios from 'axios';
@@ -33,39 +35,36 @@ export function LoginView(props) {
     };
 
     return ( //Button to switch to register not finished!
-        <Row className="login-row">
-            <Col xs={10} sm={8} md={6} className="form-col">
-                <Form className="login-form">
-                    <Form.Label className="login-intro">User Login</Form.Label>
-                    <Form.Group controlId="formBasicUsername">
-                        <Form.Label>
-                            Username
+        <Form className="login-form">
+            <Form.Label className="login-intro">Login</Form.Label>
+            <Form.Group controlId="formBasicUsername">
+                <Form.Label>
+                    Username
                 </Form.Label>
-                        <Form.Control type="text" placeholder="Enter username" value={username} autoComplete="on" onChange={(e) => setUsername(e.target.value)} />
-                    </Form.Group>
+                <Form.Control type="text" placeholder="Enter username" value={username} autoComplete="on" onChange={(e) => setUsername(e.target.value)} />
+            </Form.Group>
 
-                    <Form.Group controlId="formBasicPassword">
-                        <Form.Label>
-                            Password
+            <Form.Group controlId="formBasicPassword">
+                <Form.Label>
+                    Password
                 </Form.Label>
-                        <Form.Control type="password" placeholder="Enter password" value={password} autoComplete="off" onChange={(e) => setPassword(e.target.value)} />
-                    </Form.Group>
-                    <Form.Group>
-                        <Button className="login-button" variant="dark" type="submit" onClick={handleSubmit}>Login</Button>
-                    </Form.Group>
-                    <Form.Group>
-                        <Form.Text>Don´t have an account?</Form.Text>
-                        <Button variant="link" type="button" /*onClick={switchToRegistrationView}*/>Register</Button>
-                    </Form.Group>
-                </Form>
-            </Col>
-        </Row>
+                <Form.Control type="password" placeholder="Enter password" value={password} autoComplete="off" onChange={(e) => setPassword(e.target.value)} />
+            </Form.Group>
+            <Button className="login-button" variant="dark" type="submit" onClick={handleSubmit}>
+                Login
+            </Button>
+            <Form.Group controlId="formBasic">
+                <Form.Text className="register-text">Don´t have an account?</Form.Text>
+                <Link to={'/register'}>
+                    <Button variant="link" className="register-button" type="link">Register</Button>
+                </Link>
+            </Form.Group>
+        </Form>
     );
 }
 
 /*=================PropTypes=================*/
 LoginView.propTypes = {
-    onLoggedIn: PropTypes.func.isRequired,
-    //onClick: PropTypes.func.isRequired
+    onLoggedIn: PropTypes.func.isRequired
 };
 /*=================PropTypes=================*/
