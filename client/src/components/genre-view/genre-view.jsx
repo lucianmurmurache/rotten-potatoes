202b36b10037a90290196b1bc1884e7f95b1fe71
LucianMurmurache <48526455+lucianmurmurache@users.noreply.github.com>
2019-12-30 19:58:47 +0100

    Replaced div with Card

--- client/src/components/genre-view/genre-view.jsx
+++ client/src/components/genre-view/genre-view.jsx
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom';
 /* =============react-bootstrap-imports=============*/
 //import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-//import Card from 'react-bootstrap/Card';
+import Card from 'react-bootstrap/Card';
 //import Row from 'react-bootstrap/Row';
 //import Col from 'react-bootstrap/Col';
 //import Container from 'react-bootstrap/Container';
@@ -16,28 +16,31 @@ import Button from 'react-bootstrap/Button';
 //import axios from 'axios';
 
 export class GenreView extends React.Component {
-    constructor(props) {
-        super(props);
+    constructor() {
+        super();
         this.state = {};
     }
 
     render() {
         const { genre } = this.props;
+        if (!genre) return null;
 
         return (
-            <div className="genre-view">
-                <div className="genre-title">
-                    <h1>{genre.name}</h1>
-                </div>
-                <div className="genre-description">
-                    Description: {genre.description}
-                </div>
-                <Link to={'/'}>
-                    <Button variant="dark" className="btn">
-                        Back
+            <Card className="genre-view">
+                <Card.Body>
+                    <Card.Title className="genre-title">
+                        {genre.name}
+                    </Card.Title>
+                    <Card.Text className="genre-description">
+                        Description: {genre.description}
+                    </Card.Text>
+                    <Link to={'/'}>
+                        <Button variant="dark" className="btn">
+                            Back
                     </Button>
-                </Link>
-            </div>
+                    </Link>
+                </Card.Body>
+            </Card>
         )
     }
 }
