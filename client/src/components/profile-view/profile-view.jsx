d9bad48f78cb4eac76c7e87928fd7caa209000c1
LucianMurmurache <48526455+lucianmurmurache@users.noreply.github.com>
2019-12-30 19:45:53 +0100

    Added getUser and deleteMovie, updated card data - not finished

--- client/src/components/profile-view/profile-view.jsx
+++ client/src/components/profile-view/profile-view.jsx
@@ -7,32 +7,96 @@ import { Link } from 'react-router-dom';
 /* =============react-bootstrap-imports=============*/
 //import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-//import Card from 'react-bootstrap/Card';
+import Card from 'react-bootstrap/Card';
 //import Row from 'react-bootstrap/Row';
 //import Col from 'react-bootstrap/Col';
 //import Container from 'react-bootstrap/Container';
 /* =============react-bootstrap-imports=============*/
 
-//import axios from 'axios';
+import axios from 'axios';
 
 export class ProfileView extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
+    constructor() {
+        super();
+        this.state = {
+            username: [],
+            password: [],
+            email: [],
+            birthday: [],
+            favourites: []
+        };
+    }
+
+    getUserProfile(token) {
+        let username = localStorage.getItem('user');
+        axios.get('https://rotten-potatoes3000.herokuapp.com/user/:username', { // Might need correction: ${username}
+            headers: { Authorization: 'Bearer ${token}' }
+        })
+            .then((response) => {
+                this.setState({
+                    username: response.data.username,
+                    email: response.data.email,
+                    birthday: response.data.birthday,
+                    favourites: response.data.favourites
+                });
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    deleteFavouriteMovie(favouriteMovie) {
+        console.log('Deleting: ', favouriteMovie);
+        axios.delete('https://rotten-potatoes3000.herokuapp.com/user/:username/movies/${favouriteMovie}', { //URL may not be correct! -replace ":username" with "${username}".
+            headers: { Authorization: 'Bearer ${token}' }
+        })
+            .then((response) => {
+                this.getUserProfile(('token')); // Might need correction: (localStorage.getItem('token'))
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    }
+
+    componentDidMount() {
+        let accessToken = localStorage.getItem('token');
+        if (accessToken !== null) {
+            console.log('Access token found');
+            this.getUserProfile(accessToken);
+        }
     }
 
     render() {
-        const { profile } = this.props;
+        const { username, email, birthday, favourites } = this.props;
 
         return (
-            <div className="profile-view">
+            <Card className="profile-view">
+                <Card.Body>
+                    <Card.Title>
+                        Profile
+                        </Card.Title>
+                    <Card.Text>Username: {username}</Card.Text>
+                    <Card.Text>Email: {email}</Card.Text>
+                    <Card.Text>Birthday: {birthday}</Card.Text>
+                    <Card.Text>Favorites: {favourites}
+                        <Link>
+                            <Button variant="dark" size="sm" onClick={this.deleteFavouriteMovie(favouriteMovie)}>
+                                Delete
+                            </Button>
+                        </Link>
+                    </Card.Text>
+                    <Link to={'#'}>
+                        <Button className="update-button">
+                            Update profile
+                        </Button>
+                    </Link>
+                </Card.Body>
                 <Link to={'/'}>
                     <Button variant="dark" className="btn">
-                        Back
+                        Back to movie list
                     </Button>
                 </Link>
-            </div>
-
+            </Card>
         )
     }
 }
