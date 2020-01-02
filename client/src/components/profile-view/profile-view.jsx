abbbdc5d3f628a2cfba5236aa98f71b663edc529
LucianMurmurache <48526455+lucianmurmurache@users.noreply.github.com>
2020-01-02 19:05:12 +0100

    Updated deleteMovie, update and delete user - work in progress

--- client/src/components/profile-view/profile-view.jsx
+++ client/src/components/profile-view/profile-view.jsx
@@ -1,16 +1,11 @@
 import React from 'react';
-//import PropTypes from 'prop-types';
 import './profile-view.scss';
 
 import { Link } from 'react-router-dom';
 
 /* =============react-bootstrap-imports=============*/
-//import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-//import Row from 'react-bootstrap/Row';
-//import Col from 'react-bootstrap/Col';
-//import Container from 'react-bootstrap/Container';
 /* =============react-bootstrap-imports=============*/
 
 import axios from 'axios';
@@ -23,7 +18,7 @@ export class ProfileView extends React.Component {
             password: null,
             email: null,
             birthday: null,
-            favourites: null
+            favourites: []
         };
     }
 
@@ -45,21 +40,21 @@ export class ProfileView extends React.Component {
                 console.log(error);
             });
     }
-    /* WORK IN PROGRESS
 
-        deleteFavouriteMovie(movie._id) {
-            console.log('Deleting: ' + movie._id);
-            axios.delete('https://rotten-potatoes3000.herokuapp.com/user/${username}/movies/${movie._id}', {
-                headers: { Authorization: 'Bearer ${token}' }
+    deleteFavouriteMovie(e, movieID) {
+        e.preventDefault();
+        console.log('Deleting ' + movieID);
+        axios.delete('https://rotten-potatoes3000.herokuapp.com/user/${username}/movies/${movieID}', {
+            headers: { Authorization: 'Bearer ${token}' }
+        })
+            .then((response) => {
+                this.getUserProfile(localStorage.getItem('token'));
             })
-                .then((response) => {
-                    this.getUserProfile(localStorage.getItem('token'));
-                })
-                .catch(function (error) {
-                    console.log(error);
-                })
-        }
-    */
+            .catch(function (error) {
+                console.log(error);
+            })
+    }
+
     componentDidMount() {
         let accessToken = localStorage.getItem('token');
         if (accessToken !== null) {
@@ -71,23 +66,19 @@ export class ProfileView extends React.Component {
     }
 
     render() {
-        const { username, email, birthday, favourites } = this.props;
+        const { username, email, birthday, favourites } = this.state;
 
         return (
             <Card className="profile-view">
                 <Card.Body>
                     <Card.Title>Profile</Card.Title>
-                    <Card.Text>Username: {username}</Card.Text>
-                    <Card.Text>Email: {email}</Card.Text>
-                    <Card.Text>Birthday: {birthday}</Card.Text>
-                    <Card.Text>Favorites: {favourites}</Card.Text>
+                    <Card.Text className="profile-username">Username: {username}</Card.Text>
+                    <Card.Text className="profile-email">Email: {email}</Card.Text>
+                    <Card.Text className="profile-birthday">Birthday: {birthday}</Card.Text>
+                    <Card.Text className="profile-favourites">Favorites: {favourites}</Card.Text>
+                    <Button variant="danger" size="sm" onClick={(e) => this.deleteFavouriteMovie(movieID)}>Delete</Button>
                 </Card.Body>
 
-                <Link to={'/user/${username}'}>
-                    <Button className="edit-button" variant="dark">
-                        Edit profile
-                        </Button>
-                </Link>
                 <Link to={'/'}>
                     <Button variant="dark" className="btn">
                         Back to movie list
@@ -98,8 +89,49 @@ export class ProfileView extends React.Component {
     }
 }
 
-/*=================PropTypes=================*/
-//ProfileView.propTypes = {
-//   no props yet!
-//};
-/*=================PropTypes=================*/
+
+/* WORK IN PROGRESS
+
+   const handleUpdate = (e) => {
+        e.preventDefault();
+        axios.put('https://rotten-potatoes3000.herokuapp.com/user/${username}', {
+            username: username,
+            password: password,
+            email: email,
+            birthday: birthday
+        }, {
+            headers: { Authorization: 'Bearer ${token}' }
+        })
+        .then((response) => {
+            console.log('Your profile has been updated!')
+        })
+        .catch(function (error) {
+                console.log('Unable to update profile: ' + error);
+            })
+
+    }
+
+    const handleDelete = (e) => {
+        e.preventDefault();
+        axios.delete('https://rotten-potatoes3000.herokuapp.com/user/${username}', {
+            headers: { Authorization: 'Bearer ${token}' }
+        })
+        .then((response) => {
+            console.log('Account deleted');
+            localStorage.removeItem('user');
+            localStorage.removeItem('token');
+            window.open('/' , '_self');
+        })
+        .catch(function (error) {
+                console.log('Unable to delete profile: ' + error);
+            })
+    }
+
+    ====================BUTTONS====================
+    <Link to={'/user/${user}'}>
+        <Button className="profile-button">Profile</Button>
+    </Link>
+
+    <Button variant="light" className="logout-button" onClick={() => this.onLogout()}>Logout</Button>
+    ====================BUTTONS====================
+*/
