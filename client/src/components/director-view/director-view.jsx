b5206a0ec108da0b951598e8e9e606b21075298f
LucianMurmurache <48526455+lucianmurmurache@users.noreply.github.com>
2020-01-05 21:43:20 +0100

    Updated directors file-removed noLongerNeeded comments

--- client/src/components/director-view/director-view.jsx
+++ client/src/components/director-view/director-view.jsx
@@ -1,19 +1,12 @@
 import React from 'react';
-//import PropTypes from 'prop-types';
 import './director-view.scss';
 
 import { Link } from 'react-router-dom';
 
 /* =============react-bootstrap-imports=============*/
-//import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-//import Row from 'react-bootstrap/Row';
-//import Col from 'react-bootstrap/Col';
-//import Container from 'react-bootstrap/Container';
 /* =============react-bootstrap-imports=============*/
-
-//import axios from 'axios';
 
 export class DirectorView extends React.Component {
     constructor() {
@@ -38,7 +31,7 @@ export class DirectorView extends React.Component {
                         Biography: {director.bio}
                     </Card.Text>
                     <Link to={'/'}>
-                        <Button variant="dark" className="btn">
+                        <Button variant="outline-dark" className="btn">
                             Back
                     </Button>
                     </Link>
@@ -47,10 +40,3 @@ export class DirectorView extends React.Component {
         )
     }
 }
-
-
-/*=================PropTypes=================*/
-//DirectorView.propTypes = {
-//   no props yet!
-//};
-/*=================PropTypes=================*/
