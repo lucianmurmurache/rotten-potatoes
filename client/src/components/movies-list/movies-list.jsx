687e94ab43fea01e4ec665befa35f902a02d83cb
LucianMurmurache <48526455+lucianmurmurache@users.noreply.github.com>
2020-01-05 21:57:02 +0100

    First commit-created moviesList file

---
+++ client/src/components/movies-list/movies-list.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import './movies-list.scss';
+
+/* =============react-bootstrap-imports=============*/
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+
+/* =============react-bootstrap-imports=============*/
+
+
+import VisibilityFilterInput from '../visibility-filter-input/visibility-filter-input';
+import { MovieCard } from '../movie-card/movie-card';
+
+const mapStateToProps = state => {
+    const { visibilityFilter } = state;
+    return { visibilityFilter };
+};
+
+function MoviesList(props) {
+    const { movies, visibilityFilter } = props;
+    let filteredMovies = movies;
+
+    if (visibilityFilter !== '') {
+        filteredMovies = movies.filter(m => m.title.includes(visibilityFilter));
+    }
+
+    if (!movies) return <div className="main-view" />
+
+    return <div className="movies-list">
+        <VisibilityFilterInput
+            visibilityFilter={visibilityFilter} />
+        <Container className="movies-container">
+            <Row className="movies-row">
+                {filteredMovies.map(m => <MovieCard key={m._id} movie={m} />)}
+            </Row>
+        </Container>
+    </div>;
+}
+
+export default connect(mapStateToProps)(MoviesList);
