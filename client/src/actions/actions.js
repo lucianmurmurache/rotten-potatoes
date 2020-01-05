19f7018da943fcac598ae79e388f750c0ba78e64
LucianMurmurache <48526455+lucianmurmurache@users.noreply.github.com>
2020-01-05 21:39:15 +0100

    First commit-created actions file

--- client/src/actions/actions.js
+++ client/src/actions/actions.js
@@ -1,5 +1,6 @@
 export const SET_MOVIES = 'SET_MOVIES';
 export const SET_FILTER = 'SET_FILTER';
+export const SET_LOGGED_IN_USER = 'SET_LOGGED_IN_USER'
 
 export function setMovies(value) {
     return {
@@ -11,6 +12,13 @@ export function setMovies(value) {
 export function setFilter(value) {
     return {
         type: SET_FILTER,
+        value
+    };
+}
+
+export function setLoggedInUser(value) {
+    return {
+        type: SET_LOGGED_IN_USER,
         value
     };
 }
