8e8e907a00d5a7f55ca6c1320b8c1e3afb8ed438
LucianMurmurache <48526455+lucianmurmurache@users.noreply.github.com>
2020-01-05 21:46:34 +0100

    Removed some comments and unused imports

--- client/src/components/movie-card/movie-card.jsx
+++ client/src/components/movie-card/movie-card.jsx
@@ -5,14 +5,8 @@ import './movie-card.scss';
 import { Link } from 'react-router-dom';
 
 /* =============react-bootstrap-imports=============*/
-//import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-// import CardDeck from 'react-bootstrap/CardDeck';
-// import CardGroup from 'react-bootstrap/CardGroup';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-//import Container from 'react-bootstrap/Container';
 /* =============react-bootstrap-imports=============*/
 
 export class MovieCard extends React.Component {
@@ -20,20 +14,16 @@ export class MovieCard extends React.Component {
         const { movie } = this.props;
 
         return (
-            <Row>
-                <Col xs={6} md={4}>
-                    <Card className="movie-card" bg="light">
-                        <Card.Img variant="top" src={movie.imagePath} />
-                        <Card.Body>
-                            <Card.Title>{movie.title}</Card.Title>
-                            <Card.Text>{movie.description}</Card.Text>
-                            <Link to={'/movies/${movie._id'}>
-                                <Button variant="link">Open</Button>
-                            </Link>
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
+            <Card className="movie-card" bg="light">
+                <Card.Img variant="top" src={movie.imagePath} />
+                <Card.Body>
+                    <Card.Title>{movie.title}</Card.Title>
+                    <Card.Text>{movie.description}</Card.Text>
+                    <Link to={'/movies/${movie._id'}>
+                        <Button variant="link">Open</Button>
+                    </Link>
+                </Card.Body>
+            </Card>
         );
     }
 }
@@ -41,19 +31,9 @@ export class MovieCard extends React.Component {
 /*=================PropTypes=================*/
 MovieCard.propTypes = {
     movie: PropTypes.shape({
-        _id: PropTypes.string.isRequired,
-        title: PropTypes.string.isRequired,
-        description: PropTypes.string.isRequired,
-        genre: PropTypes.shape({
-            name: PropTypes.string.isRequired,
-            description: PropTypes.string.isRequired
-        }).isRequired,
-        director: PropTypes.shape({
-            name: PropTypes.string.isRequired,
-            bio: PropTypes.string.isRequired,
-            birth: PropTypes.string.isRequired
-        }).isRequired,
-        imagePath: PropTypes.string.isRequired
+        title: PropTypes.string,
+        description: PropTypes.string,
+        imagePath: PropTypes.string
     }).isRequired,
 };
 /*=================PropTypes=================*/
