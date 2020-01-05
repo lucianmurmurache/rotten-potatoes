8a740f92bc60baf79d4d5875225a18cdbcad095b
LucianMurmurache <48526455+lucianmurmurache@users.noreply.github.com>
2020-01-05 21:52:25 +0100

    Removed some comments and unused data, indented form

--- client/src/components/registartion-view/registration-view.jsx
+++ client/src/components/registartion-view/registration-view.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-//import PropTypes from 'prop-types';
 import './registration-view.scss';
 
 import { Link } from 'react-router-dom';
@@ -8,9 +7,6 @@ import { Link } from 'react-router-dom';
 /* =============react-bootstrap-imports=============*/
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-//import Row from 'react-bootstrap/Row';
-//import Col from 'react-bootstrap/Col';
-//import Container from 'react-bootstrap/Container';
 /* =============react-bootstrap-imports=============*/
 
 export function RegistrationView(props) {
@@ -20,7 +16,7 @@ export function RegistrationView(props) {
     const [birthday, setBirthday] = useState('');
 
 
-    const handleRegister = (e) => {
+    const handleRegister = e => {
         e.preventDefault();
         axios.post('https://rotten-potatoes3000.herokuapp.com/user', {
             username: username,
@@ -31,7 +27,7 @@ export function RegistrationView(props) {
             .then((response) => {
                 const data = response.data;
                 console.log(data);
-                window.open('/', '_self'); // The argument '_self' is needed for the ppage to open in the current tab.
+                window.open('/client', '_self');
             })
             .catch((e) => {
                 console.log('Unable to register user, try again.')
@@ -46,47 +42,74 @@ export function RegistrationView(props) {
                 <Form.Label>
                     Username
                     </Form.Label>
-                <Form.Control type="text" value={username} placeholder="Set username (min 5 characters)" onChange={(e) => setUsername(e.target.value)} />
-
+                <Form.Control
+                    type="text"
+                    value={username}
+                    placeholder="Set username (min 5 characters)"
+                    onChange={e => setUsername(e.target.value)}
+                />
             </Form.Group>
 
             <Form.Group controlId="formBasicPassword">
                 <Form.Label>
                     Password
                 </Form.Label>
-                <Form.Control type="password" value={password} placeholder="Set password (min 8 characters)" autoComplete="off" onChange={(e) => setPassword(e.target.value)} />
+                <Form.Control
+                    type="password"
+                    value={password}
+                    placeholder="Set password (min 8 characters)"
+                    autoComplete="off"
+                    onChange={e => setPassword(e.target.value)}
+                />
             </Form.Group>
 
             <Form.Group controlId="formBasicEmail">
                 <Form.Label>
                     Email
                 </Form.Label>
-                <Form.Control type="text" value={email} placeholder="Enter email address (eg: abc@123.com)" onChange={(e) => setEmail(e.target.value)} />
+                <Form.Control
+                    type="text"
+                    value={email}
+                    placeholder="Enter email address (eg: ex@ample.com)"
+                    onChange={e => setEmail(e.target.value)}
+                />
             </Form.Group>
 
             <Form.Group controlId="formBasicBirthday">
                 <Form.Label>
                     Birthday
                 </Form.Label>
-                <Form.Control type="date" value={birthday} placeholder="dd-mm-yyyy" onChange={(e) => setBirthday(e.target.value)} />
+                <Form.Control
+                    type="date"
+                    value={birthday}
+                    placeholder="dd-mm-yyyy"
+                    onChange={e => setBirthday(e.target.value)}
+                />
             </Form.Group>
-            <Button variant="dark" className="registration-button" type="submit" onClick={handleRegister}>
+
+            <Button
+                variant="outline-dark"
+                className="registration-button"
+                type="submit"
+                onClick={handleRegister}
+            >
                 Register
             </Button>
+
             <Form.Group controlId="formBasic">
                 <Form.Text className="login-text">
                     Already have an account?
                 </Form.Text>
                 <Link to={'/'}>
-                    <Button variant="link" className="login-button" type="link">Login</Button>
+                    <Button
+                        variant="link"
+                        className="login-button"
+                        type="link"
+                    >
+                        Login
+                    </Button>
                 </Link>
             </Form.Group>
         </Form>
     )
 }
-
-/*=================PropTypes=================*/
-RegistrationView.propTypes = {
-    // No props!
-};
-/*=================PropTypes=================*/
