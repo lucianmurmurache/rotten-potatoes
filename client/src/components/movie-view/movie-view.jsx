b4d7335c5c7a1f26a7c77f1e869d4c74493bb638
LucianMurmurache <48526455+lucianmurmurache@users.noreply.github.com>
2020-01-07 21:44:53 +0100

    Bug fix-corrected LinkTo, data now showing

--- client/src/components/movie-view/movie-view.jsx
+++ client/src/components/movie-view/movie-view.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import './movie-view.scss';
-import PropTypes from 'prop-types';
 
 import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
+//import { connect } from 'react-redux';
 
 /* =============react-bootstrap-imports=============*/
 import Button from 'react-bootstrap/Button';
@@ -25,34 +24,28 @@ export class MovieView extends React.Component {
             <div className="movie-view">
                 <Card className="movie-view__card">
                     <Card.Img alt="movie-poster" variant="top" className="movie-poster" src={movie.imagePath} />
-                    <Card.Title className="movie-title">
-                        {movie.title}
-                    </Card.Title>
-                    <Card.Text className="movie-description">
-                        Description: {movie.description}
-                    </Card.Text>
-                    <Link to={'/genres/${movie.genre.name}'}>
-                        <Button variant="link">Genre</Button>
-                    </Link>
-                    <Link to={'/directors/${movie.director.name}'}>
-                        <Button variant="link">Director</Button>
-                    </Link>
-                    <Link to={'/'}>
-                        <Button variant="outline-dark" className="return-button">
-                            Return to list
+                    <Card.Body>
+                        <Card.Title className="movie-title">
+                            {movie.title}
+                        </Card.Title>
+                        <Card.Text className="movie-description">
+                            Description: {movie.description}
+                        </Card.Text>
+                        <Link to={`/genres/${movie.genre.name}`}>
+                            <Button variant="link">Genre</Button>
+                        </Link>
+                        <Link to={`/directors/${movie.director.name}`}>
+                            <Button variant="link">Director</Button>
+                        </Link>
+                        <br></br>
+                        <Link to={'/'}>
+                            <Button variant="outline-dark" className="return-button">
+                                Return to list
                         </Button>
-                    </Link>
+                        </Link>
+                    </Card.Body>
                 </Card>
             </div >
         );
     }
 }
-
-/*=================PropTypes=================*/
-MovieView.propTypes = {
-    movie: PropTypes.shape({
-        title: PropTypes.string,
-        description: PropTypes.string
-    }).isRequired
-};
-/*=================PropTypes=================*/
