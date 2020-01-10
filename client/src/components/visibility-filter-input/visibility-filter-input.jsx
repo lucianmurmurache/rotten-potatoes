60f8dacb749a6f54b772d300c5429ab5906d87c6
LucianMurmurache <48526455+lucianmurmurache@users.noreply.github.com>
2020-01-10 21:41:14 +0100

    Deleted unused react-bootstrap import

--- client/src/components/visibility-filter-input/visibility-filter-input.jsx
+++ client/src/components/visibility-filter-input/visibility-filter-input.jsx
@@ -3,14 +3,15 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import './visibility-filter-input.scss';
 
-import { Link } from 'react-router-dom';
-
+/* =============react-bootstrap-imports=============*/
 import Form from 'react-bootstrap/Form';
 import Navbar from 'react-bootstrap/Navbar';
+/* =============react-bootstrap-imports=============*/
 
 import { setFilter } from '../../actions/actions';
 
 function VisibilityFilterInput(props) {
+
     return <Navbar fixed="top" bg="light" variant="light" >
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
