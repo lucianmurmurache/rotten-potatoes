970efd9d7efe31318d33b53d1f838370c9af30ee
LucianMurmurache <48526455+lucianmurmurache@users.noreply.github.com>
2020-01-11 20:38:28 +0100

    Add list of movies of each genre-view

--- client/src/components/genre-view/genre-view.jsx
+++ client/src/components/genre-view/genre-view.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './genre-view.scss';
+import PropTypes from 'prop-types';
 
 import { Link } from 'react-router-dom';
 
 /* =============react-bootstrap-imports=============*/
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 /* =============react-bootstrap-imports=============*/
 
 export class GenreView extends React.Component {
@@ -15,25 +18,65 @@ export class GenreView extends React.Component {
     }
 
     render() {
-        const { genre } = this.props;
+        const { genre, movie, movies } = this.props;
+        console.log(genre);
+        console.log(movie);
+        console.log(movies);
+
         if (!genre) return null;
 
         return (
-            <Card className="genre-view">
-                <Card.Body>
-                    <Card.Title className="genre-title">
-                        {genre.name}
-                    </Card.Title>
-                    <Card.Text className="genre-description">
-                        {genre.description}
-                    </Card.Text>
-                    <Link to={'/'}>
-                        <Button variant="outline-dark" className="btn">
-                            Back to list
-                        </Button>
-                    </Link>
-                </Card.Body>
-            </Card>
+            <div className="view">
+                <Card className="genre-view">
+                    <Card.Body>
+                        <Card.Title className="genre-title">
+                            {genre.name}
+                        </Card.Title>
+                        <Card.Text className="genre-description">
+                            {genre.description}
+                        </Card.Text>
+                        <Link to={'/'}>
+                            <Button
+                                variant="outline-dark"
+                                className="btn"
+                            >
+                                Back to list
+                            </Button>
+                        </Link>
+                    </Card.Body>
+                </Card>
+                <br></br><br></br>
+                <Container>
+                    <h2>Movies of {genre.name} genre</h2><br></br>
+                    <div className="row">
+                        {movies.map(movie => {
+                            if (movie.genre.name === genre.name) {
+                                return (
+                                    <div key={movie._id} className="genre-movies">
+                                        <Card className="movie-card">
+                                            <Card.Img variant="top" src={movie.imagePath} />
+                                            <Card.Body>
+                                                <Link className="text-muted" to={`/movies/${movie._id}`}>
+                                                    <Card.Title>{movie.title}</Card.Title>
+                                                </Link>
+                                                <Card.Text>{movie.description.substring(0, 100)}...</Card.Text>
+                                            </Card.Body>
+                                        </Card>
+                                    </div>
+                                )
+                            }
+                        })}
+                    </div>
+                </Container>
+
+            </div>
         )
     }
 }
+
+GenreView.propTypes = {
+    genre: PropTypes.shape({
+        name: PropTypes.string,
+        description: PropTypes.string
+    }).isRequired
+};
