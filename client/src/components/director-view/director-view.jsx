e7f05c2801fe13be71d0568051e2d9fc063cc340
LucianMurmurache <48526455+lucianmurmurache@users.noreply.github.com>
2020-01-13 16:41:52 +0100

    Ongoing Bug- still working on fix

--- client/src/components/director-view/director-view.jsx
+++ client/src/components/director-view/director-view.jsx
@@ -16,25 +16,27 @@ export class DirectorView extends React.Component {
     }
 
     render() {
-        const { director, movies } = this.props;
+        const { director, movies, movie } = this.props;
         console.log(director);
         console.log(movies);
 
         if (!director) return null;
-
+        // Render error between L25 and L47
         return (
             <div className="view">
-                <Card className="director-view">
-                    <Card.Body>
-                        <Card.Title className="director-title">
-                            {director.name}
-                        </Card.Title>
-                        <Card.text className="director-birth">
-                            Born in {director.birth}
-                        </Card.text>
-                        <Card.Text className="director-bio">
-                            Biography: {director.bio}
-                        </Card.Text>
+                <Container>
+                    <Card className="director-view">
+                        <Card.Body>
+                            <Card.Title className="director-title">
+                                {director.name}
+                            </Card.Title>
+                            <Card.text className="director-birth">
+                                Born in {director.birth}
+                            </Card.text>
+                            <Card.Text className="director-bio">
+                                Biography: {director.bio}
+                            </Card.Text>
+                        </Card.Body>
                         <Link to={'/'}>
                             <Button
                                 variant="outline-dark"
@@ -43,29 +45,29 @@ export class DirectorView extends React.Component {
                                 Back
                             </Button>
                         </Link>
-                    </Card.Body>
-                </Card>
-                <Container>
-                    <h2>Here are a few movies by {director.name}</h2>
-                    <div className="row d-flex">
-                        {movies.map(movie => {
-                            if (movie.director.name === director.name) {
-                                return (
-                                    <div key={movie._id}>
-                                        <Card className="director-movies">
-                                            <Card.Img variant="top" src={movie.imagePath} />
-                                            <Card.Body>
-                                                <Link to={`/movies/${movie._id}`}>
-                                                    <Card.Title>{movie.title}</Card.Title>
-                                                </Link>
-                                                <Card.Text>{movie.description.substring(0, 100)}...</Card.Text>
-                                            </Card.Body>
-                                        </Card>
-                                    </div>
-                                )
-                            }
-                        })}
-                    </div>
+                    </Card>
+                    <Container>
+                        <h2>Here are a few movies by {director.name}</h2>
+                        <div className="row">
+                            {movies.map(movie => {
+                                if (movie.director.name === director.name) {
+                                    return (
+                                        <div key={movie._id}>
+                                            <Card className="director-movies">
+                                                <Card.Img variant="top" src={movie.imagePath} />
+                                                <Card.Body>
+                                                    <Link to={`/movies/${movie._id}`}>
+                                                        <Card.Title>{movie.title}</Card.Title>
+                                                    </Link>
+                                                    <Card.Text>{movie.description.substring(0, 100)}...</Card.Text>
+                                                </Card.Body>
+                                            </Card>
+                                        </div>
+                                    )
+                                }
+                            })}
+                        </div>
+                    </Container>
                 </Container>
             </div>
         );
