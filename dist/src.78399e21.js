fb8abafb5c69feabc37d565dd408382ee7f3430a
LucianMurmurache <48526455+lucianmurmurache@users.noreply.github.com>
2020-01-13 19:36:55 +0100

    Parcel build.

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -45566,7 +45566,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58700" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59113" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
