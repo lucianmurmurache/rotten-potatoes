8d193d0c05fb76197e1a5f179aebe671c26c1d58
LucianMurmurache <48526455+lucianmurmurache@users.noreply.github.com>
2020-01-18 20:32:52 +0100

    3.5 - Final version.

--- client/src/components/profile-view/profile-view.jsx
+++ client/src/components/profile-view/profile-view.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import './profile-view.scss';
 
+import { connect } from 'react-redux';
+
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 /* =============react-bootstrap-imports=============*/
 import ListGroup from 'react-bootstrap/ListGroup';
-import Spinner from 'react-bootstrap/Spinner';
+import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import Form from 'react-bootstrap/Form';
 /* =============react-bootstrap-imports=============*/
-
-import axios from 'axios';
 
 export class ProfileView extends React.Component {
     constructor() {
@@ -22,12 +22,7 @@ export class ProfileView extends React.Component {
             password: null,
             email: null,
             birthday: null,
-            usernameNew: null,
-            passwordNew: null,
-            emailNew: null,
-            birthdayNew: null,
-            favourites: [],
-            movies: []
+            favourites: []
         };
     }
 
@@ -39,11 +34,13 @@ export class ProfileView extends React.Component {
     }
 
     getUserProfile(token) {
-        axios.get(`https://rotten-potatoes3000.herokuapp.com/user/${localStorage.getItem('user')}`, {
+        let username = localStorage.getItem('user');
+        axios.get(`https://rotten-potatoes3000.herokuapp.com/user/${username}`, {
             headers: { Authorization: `Bearer ${token}` }
         })
-            .then((response) => {
+            .then(response => {
                 this.setState({
+                    userData: response.data,
                     username: response.data.username,
                     password: response.data.password,
                     email: response.data.email,
@@ -52,140 +49,85 @@ export class ProfileView extends React.Component {
                 });
             })
             .catch(function (error) {
-                console.log(error);
+                alert('Unable to get profile data!' + error)
             });
     }
 
-    deleteUserProfile() {
-        axios.delete(`https://rotten-potatoes3000.herokuapp.com/user/${localStorage.getItem('user')}`, {
+    deleteFavouriteMovie(event, favouriteMovie) {
+        event.preventDefault();
+        let username = localStorage.getItem('user');
+        axios.delete(`https://rotten-potatoes3000.herokuapp.com/user/${username}/movies/${favouriteMovie}`, {
             headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
         })
             .then(res => {
-                confirm('Do you really want to delete your account?')
-            })
-            .then(res => {
-                console.log('User account deleted.');
-                localStorage.removeItem('user');
-                localStorage.removeItem('token');
-                window.open('/', '_self');
+                this.getUserProfile(localStorage.getItem('token'));
+                alert('Movie deleted!');
             })
             .catch(function (error) {
-                console.log('Unable to delete user account: ' + error);
+                alert('Unable to delete movie, reload the page and try again!' + error);
             });
     }
-    /*====================|=ERROR=STARTS=HERE=|====================*/
-    // deleteFavMovie does not work!! User token not recognised.
-    deleteFavouriteMovie(movieID) {
-        axios.delete(`https://rotten-potatoes3000.herokuapp.com/user/${localStorage.getItem('user')}/movies/${movieID}`, {
-            username: localStorage.getItem('user')
-        }, {
-            headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
-        })
-            .then(res => {
-                console.log('Movie deleted from favorites list');
-                alert('The movie has been deleted from your favorites list');
-            })
-            .then(res => {
-                document.location.reload(true);
-            })
-            .catch(function (error) {
-                console.log('Unable to delete movie: ' + error);
-                alert('Unable to delete movie from your favourites list. Please refresh the page and try again!');
-            });
-    }
-    /*====================|=ERROR=ENDS=HERE=|====================*/
-
-    /* OLD VERSION - ALSO NOT WORKING
-        deleteFavouriteMovie(movieID) {
-        axios.delete(`https://rotten-potatoes3000.herokuapp.com/user/${localStorage.getItem('user')}/movies/${movieID}`, {
-            headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
-        })
-            .then(res => {
-                document.location.reload(true);
-            })
-            .then(res => {
-                console.log('Movie deleted from favorites list');
-                alert('The movie has been deleted from your favorites list');
-            })
-            .catch(function (error) {
-                console.log('Unable to delete movie: ' + error);
-                alert('Unable to delete movie from your favourites list. Please refresh the page and try again!');
-            });
-    } 
-    */
 
     handleChange(event) {
         this.setState({ [event.target.name]: event.target.value });
     }
 
-    handleProfileUpdate(event) {
-        event.preventDefault();
-        axios.put(`https://rotten-potatoes3000.herokuapp.com/user/${localStorage.getItem('user')}`, {
-            username: this.state.usernameNew,
-            password: this.state.passwordNew,
-            email: this.state.emailNew,
-            birthday: this.state.birthdayNew
-        }, {
-            headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
-        })
-            .then(res => {
-                console.log('User data has been successfuly updated.');
-                alert('Profile successfuly updated!');
-                localStorage.setItem('user', this.state.usernameNew);
-            })
-            .catch(function (error) {
-                console.log('Unable to update user profile: ' + error);
-                alert('Unable to update user profile: ' + error);
-            });
-    }
-
     render() {
         const { username, email, birthday, favourites } = this.state;
-        const { movies } = this.props;
 
-        if (!username || !movies) return <div>
-            <Spinner animation="border" role="status">
-                <span className="sr-only">Loading...</span>
-            </Spinner>
-        </div>;
         return (
-            <div className="profile">
-                <Link to={'/'}>
-                    <Button
-                        variant="outline-dark"
-                        className="profile-return-button"
-                    >
-                        Back to movie list
-                    </Button>
-                </Link>
-                <Card className="profile-view">
+            <div className="profile-view">
+                <div className="update-profile-buttons">
+                    <Link to={'/'}>
+                        <Button
+                            variant="outline-dark"
+                            className="profile-view-return-button"
+                        >
+                            Back to movie list
+                        </Button>
+                    </Link>
+
+                    <Link to={`/update/:username`}>
+                        <Button
+                            variant="outline-dark"
+                            className="profile-view-update-button"
+                        >
+                            Update profile
+                        </Button>
+                    </Link>
+                </div>
+
+                <Card className="profile-view-card">
                     <Card.Body>
                         <Card.Title>Profile</Card.Title>
                         <ListGroup className="list-group-flush" variant="flush">
                             <ListGroup.Item className="profile-username">Username: {username}</ListGroup.Item>
                             <ListGroup.Item className="profile-email">Email: {email}</ListGroup.Item>
-                            <ListGroup.Item className="profile-birthday">Birthday: {birthday && birthday.slice(0, 10)}</ListGroup.Item>
+                            <ListGroup.Item className="profile-birthday">Birthday:<span>{birthday && birthday.slice(0, 10)}</span></ListGroup.Item>
                             <ListGroup.Item className="profile-favourites">Favorites:
                                 <div>
                                     {favourites.length === 0 && <p>No movies added yet.</p>}
                                     {favourites.length > 0 &&
                                         <ListGroup>
-                                            {favourites.map(movieID => (
-                                                <ListGroup.Item key={movieID} variant="flush">
-                                                    <p className="favourite-movies">
-                                                        {movies.find(movie => movie._id === movieID).title}
-                                                    </p>
-                                                    <Link to={`/movies/${movieID}`}>
-                                                        <Button size="sm" variant="outline-dark">Open</Button>
-                                                    </Link>
-                                                    <Button
-                                                        variant="outline-danger"
-                                                        className="delete-button"
-                                                        size="sm"
-                                                        onClick={e => this.deleteFavouriteMovie(e)}
-                                                    >
-                                                        Delete movie
+                                            {favourites.map(favouriteMovie => (
+                                                <ListGroup.Item key={favouriteMovie}>
+                                                    <p>
+                                                        <Link to={`/movies/${favouriteMovie}`}>
+                                                            {
+                                                                JSON.parse(localStorage.getItem('movies')).find(
+                                                                    movie => movie._id === favouriteMovie
+                                                                ).title
+                                                            }
+                                                        </Link>
+                                                        <Button
+                                                            variant="outline-danger"
+                                                            className="delete-movie-button"
+                                                            size="sm"
+                                                            onClick={e => this.deleteFavouriteMovie(event, favouriteMovie)}
+                                                        >
+                                                            Delete
                                                     </Button>
+                                                    </p>
                                                 </ListGroup.Item>)
                                             )}
                                         </ListGroup>
@@ -195,89 +137,13 @@ export class ProfileView extends React.Component {
                         </ListGroup>
                     </Card.Body>
                 </Card>
-                <br></br><br></br><br></br>
-                <Card className="profile-view">
-                    <Form className="update-profile">
-
-                        <Form.Label className="update-profile-title">Update profile data</Form.Label><br></br>
-
-                        <small className="warning">*Please ensure that you fill in all input fields prior to pressing Update.
-                            If you wish to keep some details as they are, fill in the current data again.</small><br></br><br></br>
-
-                        <Form.Group controlId="formBasicUsername">
-                            <Form.Label>Username</Form.Label>
-                            <Form.Control
-                                required
-                                type="text"
-                                placeholder="Enter a new username"
-                                name="usernameNew"
-                                autoComplete="off"
-                                onChange={event => this.handleChange(event)}
-                            >
-                            </Form.Control>
-                        </Form.Group>
-
-                        <Form.Group controlId="formBasicPassword">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control
-                                required
-                                type="password"
-                                placeholder="Enter a new password"
-                                name="passwordNew"
-                                autoComplete="off"
-                                onChange={event => this.handleChange(event)}
-                            >
-                            </Form.Control>
-                        </Form.Group>
-
-                        <Form.Group controlId="formBasicEmail">
-                            <Form.Label>Email</Form.Label>
-                            <Form.Control
-                                required
-                                type="text"
-                                placeholder="Enter a new email"
-                                name="emailNew"
-                                onChange={event => this.handleChange(event)}
-                            >
-                            </Form.Control>
-                        </Form.Group>
-
-                        <Form.Group controlId="formBasicBirthday">
-                            <Form.Label>Birthday</Form.Label>
-                            <Form.Control
-                                required
-                                type="date"
-                                placeholder="dd/mm/yyyy"
-                                name="birthdayNew"
-                                onChange={event => this.handleChange(event)}
-                            >
-                            </Form.Control>
-                        </Form.Group>
-                        <Button
-                            className="update-profile-button"
-                            vatiant="outline-dark"
-                            type="button"
-                            onClick={event => this.handleProfileUpdate(event)}
-                        >
-                            Update
-                        </Button>
-                        <br></br><br></br>
-                        <Form.Label className="delete-profile-title">Delete account</Form.Label><br></br>
-                        <small>
-                            *Should you decide to delete your profile, please be aware that once you do,
-                            your data will be deleted without any backup! It is not possible to revert this action!
-                        </small>
-                        <Button
-                            size="sm"
-                            variant="outline-danger"
-                            className="delete-profile-button"
-                            onClick={() => this.deleteUserProfile()}
-                        >
-                            Delete Profile
-                        </Button>
-                    </Form>
-                </Card>
-            </div>
+            </div >
         );
     }
 }
+
+let mapStateToProps = state => {
+    return { movies: state.movies, loggedInUser: state.loggedInUser }
+}
+
+export default connect(mapStateToProps)(ProfileView);
